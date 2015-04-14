2e8cf222968157fcf94d3d7b89eb9dd607c3cb3e
Darren M <aeneascorrupt@gmail.com>
2015-04-13 19:33:41 -0700

    initial main view

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 exports.register = function (server, options, next) {
+  server.views({
+    engines: {
+      html: {
+        module: require('handlebars'),
+        compileMode: 'sync'
+      }
+    },
+    path: './views'
+  });
+
   server.route({
     method: 'GET',
     path: '/status',
     handler: function (request, reply) {
       reply({status: 'OK'});
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: {
+      view: 'main'
     }
   });
 
